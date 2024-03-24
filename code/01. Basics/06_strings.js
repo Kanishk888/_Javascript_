622d7c0cbc6cd47a2c08f03fd4cad8e8eccc990f
Kanishk Gupta <66403977+Kanishk888@users.noreply.github.com>
2024-03-24 14:07:11 +0000

    changed

---
+++ code/01. Basics/06_strings.js
@@ -0,0 +1,15 @@
+const name = "hitesh";
+const repoCount = 50;
+
+console.log(name + repoCount + " Value"); // this way is deprecated
+
+/*string interpolation[`${//placeholder}`] */
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+/**
+ * Ways of declaring strings:
+ * 1. const/let variable = "string";
+ * 2. const/let variable = new String("string");
+ *
+ * String deconstructor provides some additional methods/functions
+ */
