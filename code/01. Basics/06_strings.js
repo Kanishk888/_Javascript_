490ad89d7a2bd1239e9c9b1adda4f15934043ff8
Kanishk Gupta <66403977+Kanishk888@users.noreply.github.com>
2024-03-26 12:32:29 +0000

    changed

--- code/01. Basics/06_strings.js
+++ code/01. Basics/06_strings.js
@@ -13,3 +13,26 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
  *
  * String constructor provides some additional methods/functions, these methods are used to get additional information about string and also used to manipulate it.
  */
+
+const gameName = new String("hitesh-hc");
+
+console.log(gameName[0]); //
+console.log(gameName.__proto__);
+
+// .length method:
+console.log(gameName.length);
+
+// toUpperCase:
+console.log(gameName.toUpperCase());
+
+// charAt:
+console.log(gameName.charAt(3));
+
+// indexOf:
+console.log(gameName.indexOf("e"));
+
+// substring:
+const newString = gameName.substring(0, 4);
+console.log(newString); // 4 is exclusive
+
+//
